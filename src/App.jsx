351bb89800dc3f5b93644e48b47e78370db22bcd
Johnny Ramsay <johnnyramsay@Johnnys-Air.lan>
2024-03-05 17:46:48 +0000

    add folders for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Home from "./features/home/Home";
+
 function App() {
-  return <div>Hello Vite!</div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/features/home/Home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div>
+      <h1>
+        Great coffee made simple.
+        <br />
+        Start your mornings with the world’s best coffees. Try our expertly
+        curated artisan coffees from our best roasters delivered directly to
+        your door, at your schedule.
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
